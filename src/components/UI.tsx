f8a90a14375942b0fd22a603e88f71f54dd08566
Mohammad karbalaiemehdi <mohammadkarbalaiemehdi@outlook.com>
2024-10-20 15:24:19 +0330

    fix-callingcode-countryname

--- src/components/UI.tsx
+++ src/components/UI.tsx
@@ -1,106 +1,106 @@
-import React from "react";  
-import { IWheather } from "../types";  
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';  
+import React from "react";
+import { IWheather } from "../types";
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
-interface IWheatherUIProps extends IWheather {  
-    city: string; // برای دریافت city  
-    setCity: React.Dispatch<React.SetStateAction<string>>; // برای بروزرسانی city  
-    windSpeed: number;  
-    temperature: number;  
-    humidity: number;  
-    visibility: number;  
-    weatherIcon: string;  
-    population: number;  
-    capital: string;  
-    region: string;  
-    languages: string;  
-    timezones: string;  
-    callingCode: number;  
-    name:string
-    coordinates: { lat: number; lon: number };  
-}  
+interface IWheatherUIProps extends IWheather {
+    city: string;
+    setCity: React.Dispatch<React.SetStateAction<string>>;
+    windSpeed: number;
+    temperature: number;
+    humidity: number;
+    visibility: number;
+    weatherIcon: string;
+    population: number;
+    capital: string;
+    region: string;
+    languages: string;
+    timezones: string;
+    callingCode: string;
+    name: string; // نام کامل کشور
+    coordinates: { lat: number; lon: number };
+}
 
-export const IWheatherUI: React.FC<IWheatherUIProps> = ({  
-    sys: { country },  
-    city,  
-    setCity,  
-    windSpeed,  
-    temperature,  
-    humidity,  
-    visibility,  
-    weatherIcon,  
-    population,  
-    capital,  
-    region,  
-    languages,  
-    timezones,  
-    callingCode,  
-    coordinates,   
+export const IWheatherUI: React.FC<IWheatherUIProps> = ({
+    sys: { country },
+    city,
+    setCity,
+    windSpeed,
+    temperature,
+    humidity,
+    visibility,
+    weatherIcon,
+    population,
+    capital,
+    region,
+    languages,
+    timezones,
+    callingCode,
+    coordinates,
     name,
-}) => {  
-  return (  
-    <div className="flex flex-col gap-2">  
-      <div className="bg-slate-700 py-2">  
-        <p className="text-yellow-500 py-2 text-2xl font-semibold">  
-          Learning how to work with APIs  
-        </p>  
-        <input  
-            type="text"  
-            value={city}  
-            onChange={(e) => setCity(e.target.value)} // بروزرسانی city  
-            placeholder="Enter city..."  
-            className="w-96 px-4 uppercase py-1 rounded-md text-start "  
-        />  
-      </div>  
-      <div className="grid grid-cols-3 gap-4">  
-        <div className="flex flex-col bg-slate-600 text-white items-start justify-start px-2 py-2">  
-          <h1 className="font-semibold text-2xl py-3 ">{country}</h1>  
-          <p className="text-lg">Native Name :{name}</p>  
-          <p className="text-lg">Capital : {capital}</p>  
-          <p className="text-lg">Region : {region}</p>  
-          <p className="text-lg">Population : {population}</p>  
-          <p className="text-lg">Languages : {languages}</p>  
-          <p className="text-lg">Time Zone :{timezones}</p>  
-        </div>  
-        <div className="flex flex-col justify-center items-center ">  
-          <span className="uppercase text-3xl bg-slate-800 text-white py-4 w-full">  
-            Calling code  
-          </span>  
-          <span className="text-3xl bg-yellow-500 text-white py-[70px] w-full h-full">  
-           {callingCode}   
-          </span>  
-        </div>  
-        <div className="w-full border">  
-          <img src={`https://flagcdn.com//w160/${country.toLowerCase()}.png`} alt={`${country} flag`} className="mr-2 w-full h-full" style={{ width: "400px", height: "295px" }} />  
-        </div>  
-      </div>  
+}) => {
+    return (
+        <div className="flex flex-col gap-2">
+            <div className="bg-slate-700 py-2">
+                <p className="text-yellow-500 py-2 text-2xl font-semibold">
+                    Learning how to work with APIs
+                </p>
+                <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    placeholder="Enter city..."
+                    className="w-96 px-4 uppercase py-1 rounded-md text-start "
+                />
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <div className="flex flex-col bg-slate-600 text-white items-start justify-start px-2 py-2">
+                    <h1 className="font-semibold text-2xl py-3 ">{name}</h1> {/* نام کشور */}
+                    <p className="text-lg">Native Name: {name}</p>
+                    <p className="text-lg">Capital: {capital}</p>
+                    <p className="text-lg">Region: {region}</p>
+                    <p className="text-lg">Population: {population}</p>
+                    <p className="text-lg">Languages: {languages}</p>
+                    <p className="text-lg">Time Zone: {timezones}</p>
+                </div>
+                <div className="flex flex-col justify-center items-center ">
+                    <span className="uppercase text-3xl bg-slate-800 text-white py-4 w-full">
+                        Calling Code
+                    </span>
+                    <span className="text-3xl bg-yellow-500 text-white py-[70px] w-full h-full">
+                        {callingCode}
+                    </span>
+                </div>
+                <div className="w-full border">
+                    <img src={`https://flagcdn.com/${country.toLowerCase()}.svg`} alt={`${country} flag`} className="mr-2 w-full h-full" style={{ width: "400px", height: "295px" }} />
+                </div>
+            </div>
 
-      <div className="flex gap-6">  
-        <div className="w-2/6 flex flex-col border items-start justify-start">  
-          <span className="w-full uppercase text-yellow-400 bg-slate-600">  
-            <p className="font-semibold text-2xl py-3 px-3">  
-              Capital Weather Report  
-            </p>  
-          </span>  
+            <div className="flex gap-6">
+                <div className="w-2/6 flex flex-col border items-start justify-start">
+                    <span className="w-full uppercase text-yellow-400 bg-slate-600">
+                        <p className="font-semibold text-2xl py-3 px-3">
+                            Capital Weather Report
+                        </p>
+                    </span>
 
-          <span className="px-2 py-4 flex flex-col items-start justify-start">  
-            <img src={weatherIcon} alt="" className="w-3/6 ml-36 -mr-2 mb-5" />  
-            <p className="text-lg">Wind Speed: {windSpeed} m/s</p>  
-            <p className="text-lg">Temperature: {temperature} °C</p>  
-            <p className="text-lg">Humidity: {humidity} %</p>  
-            <p className="text-lg">Visibility: {visibility} m</p>  
-          </span>  
-        </div>  
-        <div className="w-4/6 border" id="map">  
-          <MapContainer center={[coordinates.lat, coordinates.lon]} zoom={7} style={{ height: "400px", width: "100%" }}>  
-              <TileLayer  
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"  
-                  attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"  
-              />  
-              <Marker position={[coordinates.lat, coordinates.lon]} />  
-          </MapContainer>  
-        </div>  
-      </div>  
-    </div>  
-  );  
+                    <span className="px-2 py-4 flex flex-col items-start justify-start">
+                        <img src={weatherIcon} alt="" className="w-3/6 ml-36 -mr-2 mb-5" />
+                        <p className="text-lg">Wind Speed: {windSpeed} m/s</p>
+                        <p className="text-lg">Temperature: {temperature} °C</p>
+                        <p className="text-lg">Humidity: {humidity} %</p>
+                        <p className="text-lg">Visibility: {visibility} m</p>
+                    </span>
+                </div>
+                <div className="w-4/6 border" id="map">
+                    <MapContainer center={[coordinates.lat, coordinates.lon]} zoom={7} style={{ height: "400px", width: "100%" }}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                        />
+                        <Marker position={[coordinates.lat, coordinates.lon]} />
+                    </MapContainer>
+                </div>
+            </div>
+        </div>
+    );
 };
--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,88 +1,117 @@
-import React, { useState, useEffect, useCallback } from "react";  
-import axios from "axios";  
-import { IWheatherUI } from "../components/UI";   
-import { IWheather } from "../types";    
-import useDebounce from "../Hooks/useDebounce";  
-import "leaflet/dist/leaflet.css";  
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import { IWheatherUI } from "../components/UI";
+import { IWheather } from "../types";
+import useDebounce from "../Hooks/useDebounce";
+import "leaflet/dist/leaflet.css";
 
-interface Country {  
-    cca2: string;  
-    capital: string[];  
-    population: number;  
-    region: string;  
-    languages: { [key: string]: string };   
-    timezones: string[];     
-}  
+interface Country {
+    cca2: string;
+    capital: string[];
+    population: number;
+    region: string;
+    languages: { [key: string]: string };
+    timezones: string[];
+    idd: {
+        root: string;
+        suffixes: string[];
+    };
+    translations: {
+        per: {
+            official: string;
+        };
+    };
+    name: {
+        nativeName: {
+            fas: {
+                official: string;
+            };
+        };
+    };
+}
 
-const WeatherApp: React.FC = () => {  
-    const [weatherData, setWeatherData] = useState<IWheather | null>(null);  
-    const [city, setCity] = useState<string>("Tehran");  
-    const [coordinates, setCoordinates] = useState<{ lat: number; lon: number }>({ lat: 35.6892, lon: 51.3890 });  
-    const [countryInfo, setCountryInfo] = useState<{   
-        capital: string;   
-        population: number;   
-        region: string;   
-        languages: string;  
-        timezones: string;  
-        callingCode: string;      
-    } | null>(null);  
+const WeatherApp: React.FC = () => {
+    const [weatherData, setWeatherData] = useState<IWheather | null>(null);
+    const [city, setCity] = useState<string>("Tehran");
+    const [coordinates, setCoordinates] = useState<{ lat: number; lon: number }>({ lat: 35.6892, lon: 51.3890 });
+    const [countryInfo, setCountryInfo] = useState<{
+        capital: string;
+        population: number;
+        region: string;
+        languages: string;
+        timezones: string;
+        callingCode: string;
+        countryName: string; // نام کامل کشور
+    } | null>(null);
+    const [error, setError] = useState<string | null>(null);
 
-    const debouncedCity = useDebounce(city, 300);  
+    const debouncedCity = useDebounce(city, 300);
 
-    const fetchWeatherData = useCallback(async (cityName: string) => {  
-        const apiKey = '6640f904052d9b03f1efd12fce643e70';   
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);  
-        setWeatherData(response.data);  
+    const fetchWeatherData = useCallback(async (cityName: string) => {
+        const apiKey = '6640f904052d9b03f1efd12fce643e70';
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric&lang=fa`);
+        
+        setWeatherData(response.data);
+        const countryCode = response.data.sys.country;
+        fetchCountryInfo(countryCode);
+        setCoordinates({ lat: response.data.coord.lat, lon: response.data.coord.lon });
+    }, []);
 
-        const countryCode = response.data.sys.country;  
-        fetchCountryInfo(countryCode);  
-        setCoordinates({ lat: response.data.coord.lat, lon: response.data.coord.lon });  
-    }, []);  
+    const fetchCountryInfo = async (countryCode: string) => {
+        try {
+            const response = await axios.get(`https://restcountries.com/v3.1/all`);
+            const countryData = response.data.find((country: Country) => country.cca2 === countryCode);
 
-    const fetchCountryInfo = async (countryCode: string) => {  
-        const response = await axios.get(`https://restcountries.com/v3.1/all`);  
-        const countryData = response.data.find((country: Country) => country.cca2 === countryCode);  
-        if (countryData) {  
-            setCountryInfo({  
-                capital: countryData.capital ? countryData.capital[0] : "",  
-                population: countryData.population,  
-                region: countryData.region,   
-                languages: Object.values(countryData.languages || {}).join(", "),   
-                timezones: countryData.timezones.join(", "),   
-                callingCode: `+${countryData.cca2}`   
-            });  
-        }  
-    };  
+            if (countryData) {
+                setCountryInfo({
+                    capital: countryData.capital ? countryData.capital[0] : "",
+                    population: countryData.population,
+                    region: countryData.region,
+                    languages: Object.values(countryData.languages || {}).join(", "),
+                    timezones: countryData.timezones.join(", "),
+                    callingCode: countryData.idd.root + (countryData.idd.suffixes ? countryData.idd.suffixes[0] : ''),
+                    countryName: countryData.translations?.per?.official || countryData.name.nativeName?.fas?.official || "Unknown Country",
+                });
+            } else {
+                throw new Error("Country not found");
+            }
+        } catch (err) {
+            console.error("Error fetching country information:", err);
+            setError("Error fetching country information. Please try again.");
+        }
+    };
 
-    useEffect(() => {  
-        if (debouncedCity) {  
-            fetchWeatherData(debouncedCity);  
-        }  
-    }, [debouncedCity, fetchWeatherData]);  
+    useEffect(() => {
+        if (debouncedCity) {
+            fetchWeatherData(debouncedCity);
+        }
+    }, [debouncedCity, fetchWeatherData]);
 
-    return (  
-        <div>  
-            {weatherData && countryInfo && (  
-                <IWheatherUI  
-                    city={city}   
-                    setCity={setCity}   
-                    coordinates={coordinates}   
-                    {...weatherData}  
-                    windSpeed={weatherData.wind.speed}  
-                    temperature={weatherData.main.temp}  
-                    humidity={weatherData.main.humidity}  
-                    visibility={weatherData.visibility}  
-                    weatherIcon={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}  
-                    population={countryInfo.population}  
-                    capital={countryInfo.capital}  
-                    region={countryInfo.region}  
-                    languages={countryInfo.languages}  
-                    timezones={countryInfo.timezones}  
-                    callingCode={countryInfo.callingCode}  
-                />  
-            )}  
-        </div>  
-    );  
-};  
+    return (
+        <div>
+            {error && <p className="text-red-500">{error}</p>}
+            {weatherData && countryInfo && (
+                <IWheatherUI
+                    city={city}
+                    setCity={setCity}
+                    coordinates={coordinates}
+                    {...weatherData}
+                    windSpeed={weatherData.wind.speed}
+                    temperature={weatherData.main.temp}
+                    humidity={weatherData.main.humidity}
+                    visibility={weatherData.visibility}
+                    weatherIcon={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+                    population={countryInfo.population}
+                    capital={countryInfo.capital}
+                    region={countryInfo.region}
+                    languages={countryInfo.languages}
+                    timezones={countryInfo.timezones}
+                    callingCode={countryInfo.callingCode}
+                    name={countryInfo.countryName} // نام کشور
+                />
+            )}
+        </div>
+    );
+};
 
 export default WeatherApp;
